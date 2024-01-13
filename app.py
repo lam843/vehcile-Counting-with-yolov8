fef320754f6e37960fbcb336bc829cc04bd5932f
Lamya Alahyane <78732216+lam843@users.noreply.github.com>
2024-01-13 11:52:36 +0100

    done

---
+++ app.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+import streamlit as st
+
+import config
+from utils import load_model, infer_uploaded_image, infer_uploaded_video, infer_uploaded_webcam
+
+# setting page layout
+st.set_page_config(
+    page_title="Vehicle counting with YOLOv8",
+    page_icon="🚗",
+    layout="wide",
+    initial_sidebar_state="expanded"
+    )
+
+# main page heading
+st.title("Vehicle counting with YOLOv8")
+
+# sidebar
+st.sidebar.header("YOLO Model Config")
+
+# model options
+task_type = st.sidebar.selectbox(
+    "Select Task",
+    ["Detection"]
+)
+
+model_type = None
+if task_type == "Detection":
+    model_type = st.sidebar.selectbox(
+        "Select Model",
+        config.DETECTION_MODEL_LIST
+    )
+else:
+    st.error("Currently only 'Detection' function is implemented")
+
+confidence = float(st.sidebar.slider(
+    "Select Model Confidence", 30, 100, 50)) / 100
+
+model_path = ""
+if model_type:
+    model_path = Path(config.DETECTION_MODEL_DIR, str(model_type))
+else:
+    st.error("Please Select Model in Sidebar")
+
+# load pretrained DL model
+try:
+    model = load_model(model_path)
+except Exception as e:
+    st.error(f"Unable to load model. Please check the specified path: {model_path}")
+
+# image/video options
+st.sidebar.header("Image/Video")
+source_selectbox = st.sidebar.selectbox(
+    "Select Source",
+    config.SOURCES_LIST
+)
+
+source_img = None
+if source_selectbox == config.SOURCES_LIST[0]: # Image
+    infer_uploaded_image(confidence, model)
+elif source_selectbox == config.SOURCES_LIST[1]: # Video
+    infer_uploaded_video(confidence, model)
+else:
+    st.error("Currently only 'Image' and 'Video' source are implemented")
